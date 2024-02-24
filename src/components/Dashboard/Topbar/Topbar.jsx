64e51fcc2b3e4d7aa1d37f2eb1635817dd3cc77a
Soham Pagi <soham_pagi@persistent.com>
2024-02-24 10:41:40 +0530

    Add Topbar

--- src/components/Dashboard/Topbar/Topbar.jsx
+++ src/components/Dashboard/Topbar/Topbar.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 import style from './Topbar.module.css';
 
+import Button from '../../Button/Button';
+
 function Topbar({ title }) {
     return (
-        <div>
+        <div className={style.topbar}>
             <div>{title}</div>
-            <div></div>
+            <Button name={"Quick Action"} />
         </div>
     )
 }
