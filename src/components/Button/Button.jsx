04cc6d729437038361c44f722d2e7398aae99801
Amey Sawalkar <ameykishor@gmail.com>
2024-02-24 10:59:27 +0530

    Button add

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 function Button({ name,handleClick }) {
     return (
-        <button className="bg-[#E89058] rounded-[10px] h[80px] px-8">Button</button>
+        <button className="bg-[#E89058] rounded-[10px] py-1 px-8 text-white font-medium">{name}</button>
     );
 }
 
